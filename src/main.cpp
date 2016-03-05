352e99dd01c26d4ab6ff883ef8108bab0747f8ef
John J <jastadj@gmail.com>
2016-03-04 16:00:56 -0800

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 #include <string>
 #include <cmath>
 #include <ctime>
-#include "oolua.h"
+#include "lua.hpp"
+//#include "oolua.h"
+//using namespace OOLUA
 
-int getRanValue()
+int getRandom()
 {
-    return rand()%10+1;
+
 }
 
 void luaPrint(char *msg)
@@ -18,6 +20,18 @@ int main(int argc, char *argv[])
 {
     srand(time(NULL));
 
+    //create lua state
+    lua_State *L = luaL_newstate();
+
+    //load file
+    if(luaL_loadfile(L,"luatest.lua")) std::cout << "Error loading lua file.\n";
+
+    lua_
+
+
+    lua_pcall(L, 0,0,0);
+
+    lua_close(L);
 
     return 0;
 }
